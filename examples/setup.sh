29cee59935b5bfe74beb350e71fb31d14419b560
Jumana Dakka <jumanadakka@gmail.com>
2017-11-30 22:56:43 -0500

    added setup.sh for exporting before running

---
+++ examples/setup.sh
@@ -0,0 +1,11 @@
+export RADICAL_PILOT_DBURL="mongodb://jdakka:mongo@ds231715.mlab.com:31715/new_htbac"
+
+export SAGA_PTY_SSH_TIMEOUT=2000
+export RADICAL_PILOT_PROFILE=True
+export RADICAL_ENMD_PROFILE=True
+export RADICAL_ENMD_PROFILING=1
+export RP_ENABLE_OLD_DEFINES=True
+export PATH=/usr/sbin:$PATH
+
+
+myproxy-logon -l <username> -s tfca.ncsa.illinois.edu
