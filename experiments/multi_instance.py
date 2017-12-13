ccf30417e0623367a64a69bc50a83417b6d0b0d0
Kristof Farkas-Pall <kristof.farkas-pall.14@ucl.ac.uk>
2017-12-13 16:56:46 +0000

    Add new experiment desc

--- experiments/multi_instance.py
+++ experiments/multi_instance.py
@@ -21,14 +21,4 @@ def main():
 
 
 if __name__ == '__main__':
-    import os
-
-    os.environ['RADICAL_ENTK_VERBOSE'] = 'INFO'
-    os.environ['RADICAL_PILOT_DBURL'] = 'mongodb://radical:fg*2GT3^eB@crick.chem.ucl.ac.uk:27017/admin'
-    os.environ['RP_ENABLE_OLD_DEFINES'] = 'True'
-    os.environ['SAGA_PTY_SSH_TIMEOUT'] = '2000'
-    os.environ['RADICAL_PILOT_PROFILE'] = 'True'
-    os.environ['RADICAL_ENMD_PROFILE'] = 'True'
-    os.environ['RADICAL_ENMD_PROFILING'] = '1'
-
     main()
