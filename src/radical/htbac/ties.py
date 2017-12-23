1b4040cce5be6f3b211dad6b2047546d8417dde2
Jumana Dakka <jumanadakka@gmail.com>
2017-12-23 16:42:52 -0500

    took out analysis

--- src/radical/htbac/ties.py
+++ src/radical/htbac/ties.py
@@ -100,60 +100,6 @@ class Ties(object):
 
             pipeline.add_stages(stage)
 
-            # Analysis stage
-            # ==============
-            analysis = Stage()
-            analysis.name = 'analysis'
-
-        for replica in range(self.number_of_replicas):
-            for system in self.systems:
-                self.box = pmd.amber.AmberAsciiRestart('systems/ties{lig}/{s}/build/{s}-complex.crd'.format(lig=self.ligand, s=system)).box            
-                analysis_task = Task()
-                analysis_task.name = 'system_{}_replica_{}'.format(system,replica)
-
-                analysis_task.arguments += ['-d', '*ti.out', '>', 'dg_{}.out'.format(analysis_task.name)]
-                analysis_task.executable = [NAMD_TI_ANALYSIS]
-
-                analysis_task.mpi = False
-                analysis_task.cores = 1
-
-                production_stage = pipeline.stages[-1]
-                production_tasks = [t for t in production_stage.tasks if analysis_task.name in t.name]
-                links = ['$Pipeline_{}_Stage_{}_Task_{}/alch_{}_ti.out'.format(pipeline.uid, production_stage.uid, t.uid, t.name.split('_lambda_')[-1]) for t in production_tasks]
-                analysis_task.link_input_data = links
-
-                analysis.add_tasks(analysis_task)
-
-        pipeline.add_stages(analysis)
-
-        # Averaging stage
-        # ===============
-        average = Stage()
-        average.name = 'average'
-
-        for system in self.systems:
-            self.box = pmd.amber.AmberAsciiRestart('systems/ties{lig}/{s}/build/{s}-complex.crd'.format(lig=self.ligand, s=system)).box            
-
-            average_task = Task()
-            average_task.name = 'average_dg'
-            average_task.arguments = ['-1 --quiet dg_* > dgs.out']  # .format(pipeline.uid)]
-            average_task.executable = ['head']
-
-            average_task.mpi = False
-            average_task.cores = 1
-
-            previous_stage = pipeline.stages[-1]
-            previous_tasks = previous_stage.tasks
-
-
-            links = ['$Pipeline_{}_Stage_{}_Task_{}/dg_{}.out'.format(pipeline.uid, previous_stage.uid, t.uid,
-                                                                      t.name) for t in previous_tasks]
-            average_task.link_input_data = links
-            #average_task.download_output_data = ['dgs.out']  # .format(pipeline.uid)]
-
-            average.add_tasks(average_task)
-        pipeline.add_stages(average)
-
         print 'TIES pipeline has', len(pipeline.stages), 'stages. Tasks counts:', [len(s.tasks) for s in pipeline.stages]
         return pipeline
 
