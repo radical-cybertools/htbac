45269103b97b1c876517310bc437357726db3e07
Jumana Dakka <jumanadakka@gmail.com>
2018-01-11 12:28:02 -0500

    updated dependency links, README

--- setup.py
+++ setup.py
@@ -201,15 +201,15 @@ setup_args = {
     'install_requires'  :  ['radical.utils==master',
                              'saga-python==master',
                              'radical.pilot==master',
-                             'radical.entk==jdakka',
+                             'radical.entk==jdakka-resubmission',
                              'setuptools>=1',
                              'pika'],
     
     
-     'dependency_links': ['git+https://github.com/radical-cybertools/radical.utils.git@master#egg=radical.utils-rc',
-                          'git+https://github.com/radical-cybertools/saga-python.git@master#egg=saga-python-rc',
-                          'git+https://github.com/radical-cybertools/radical.pilot.git@master#egg=radical.pilot-rc',
-                          'git+https://github.com/vivek-bala/radical.entk.git@experiment/jdakka-resubmission#egg=radical.entk-jdakka'],
+     'dependency_links': ['git+https://github.com/radical-cybertools/radical.utils.git@master#egg=radical.utils-master',
+                          'git+https://github.com/radical-cybertools/saga-python.git@master#egg=saga-python-master',
+                          'git+https://github.com/radical-cybertools/radical.pilot.git@master#egg=radical.pilot-master',
+                          'git+https://github.com/vivek-bala/radical.entk.git@experiment/jdakka-resubmission#egg=radical.entk-jdakka-resubmission'],
   
 
     'zip_safe'          : False,
