df43065c32375b99b0ebb6caade703e912d7b4e4
Jumana Dakka <jumanadakka@gmail.com>
2018-03-21 15:53:16 -0400

    more system changes

--- src/radical/htbac/ties.py
+++ src/radical/htbac/ties.py
@@ -16,7 +16,7 @@ _full_steps = dict(min=1000, eq1=30000, eq2=970000, prod=2000000)
 class Ties(object):
 
     def __init__(self, number_of_replicas, number_of_windows=0, additional=list(),
-                 system=list(), workflow=None, cores=32, ligand=False, full=False):
+                 system, workflow=None, cores=32, ligand=False, full=False):
 
         self.number_of_replicas = number_of_replicas
         self.lambdas = np.linspace(0.0, 1.0, number_of_windows, endpoint=True)
@@ -24,7 +24,7 @@ class Ties(object):
         self.ligand = '-ligands' if ligand else ''
         self.step_count = _full_steps if full else _reduced_steps
         
-        self.systems = system
+        self.system = system
         print "Lambda Windows: ", self.lambdas
         print "step count: ", self.step_count 
 
@@ -56,45 +56,45 @@ class Ties(object):
             stage.name = step
             print stage.name
 
-            for system in self.systems:
-                box = pmd.amber.AmberAsciiRestart('systems/ties{lig}/{s}/build/{s}-complex.crd'.format(lig=self.ligand, s=system)).box
-                    
-                for replica in range(self.number_of_replicas):
-                    for ld in self.lambdas:
-                    
-                        task = Task()
-                        task.name = 'system_{}_replica_{}_lambda_{}'.format(system, replica, ld)
+        
+            box = pmd.amber.AmberAsciiRestart('systems/ties{lig}/{s}/build/{s}-complex.crd'.format(lig=self.ligand, s=self.system)).box
+                
+            for replica in range(self.number_of_replicas):
+                for ld in self.lambdas:
+                
+                    task = Task()
+                    task.name = 'system_{}_replica_{}_lambda_{}'.format(self.system, replica, ld)
 
-                        task.arguments += ['ties-{}.conf'.format(stage.name)]
-                        task.copy_input_data = ['$SHARED/ties-{}.conf'.format(stage.name)]
-                        task.executable = [NAMD2]
+                    task.arguments += ['ties-{}.conf'.format(stage.name)]
+                    task.copy_input_data = ['$SHARED/ties-{}.conf'.format(stage.name)]
+                    task.executable = [NAMD2]
 
-                        task.mpi = True
-                        task.cores = self.cores
+                    task.mpi = True
+                    task.cores = self.cores
 
-                        links = []
-                        links += ['$SHARED/{}-complex.top'.format(system), '$SHARED/{}-tags.pdb'.format(system)]
-                        if self.workflow.index(step):
-                            previous_stage = pipeline.stages[-1]
-                            previous_task = next(t for t in previous_stage.tasks if t.name == task.name)
-                            path = '$Pipeline_{}_Stage_{}_Task_{}/'.format(pipeline.uid, previous_stage.uid, previous_task.uid)
-                            links += [path+previous_stage.name+suffix for suffix in _simulation_file_suffixes]
-                        else:
-                            links += ['$SHARED/{}-complex.pdb'.format(system)]
+                    links = []
+                    links += ['$SHARED/{}-complex.top'.format(self.system), '$SHARED/{}-tags.pdb'.format(self.system)]
+                    if self.workflow.index(step):
+                        previous_stage = pipeline.stages[-1]
+                        previous_task = next(t for t in previous_stage.tasks if t.name == task.name)
+                        path = '$Pipeline_{}_Stage_{}_Task_{}/'.format(pipeline.uid, previous_stage.uid, previous_task.uid)
+                        links += [path+previous_stage.name+suffix for suffix in _simulation_file_suffixes]
+                    else:
+                        links += ['$SHARED/{}-complex.pdb'.format(self.system)]
 
-                        print "Linking files:", links
-                        task.link_input_data = links
+                    print "Linking files:", links
+                    task.link_input_data = links
 
-                        task.pre_exec += ["sed -i 's/BOX_X/{}/g' *.conf".format(box[0]),
-                                          "sed -i 's/BOX_Y/{}/g' *.conf".format(box[1]),
-                                          "sed -i 's/BOX_Z/{}/g' *.conf".format(box[2]),
-                                          "sed -i 's/SYSTEM/{}/g' *.conf".format(system)]
+                    task.pre_exec += ["sed -i 's/BOX_X/{}/g' *.conf".format(box[0]),
+                                      "sed -i 's/BOX_Y/{}/g' *.conf".format(box[1]),
+                                      "sed -i 's/BOX_Z/{}/g' *.conf".format(box[2]),
+                                      "sed -i 's/SYSTEM/{}/g' *.conf".format(self.system)]
 
-                        task.pre_exec += ["sed -i 's/STEP/{}/g' *.conf".format(self.step_count[step])]
+                    task.pre_exec += ["sed -i 's/STEP/{}/g' *.conf".format(self.step_count[step])]
 
-                        task.pre_exec += ["sed -i 's/LAMBDA/{}/g' *.conf".format(ld)]
+                    task.pre_exec += ["sed -i 's/LAMBDA/{}/g' *.conf".format(ld)]
 
-                        stage.add_tasks(task)
+                    stage.add_tasks(task)
 
             pipeline.add_stages(stage)
             
@@ -155,13 +155,11 @@ class Ties(object):
     @property
     def input_data(self):
         files = ['default_configs/ties-{}.conf'.format(step) for step in self.workflow]
-        for system in self.systems:
-            files += ['systems/ties{lig}/{s}/build/{s}-complex.pdb'.format(lig=self.ligand, s=system)]
-            files += ['systems/ties{lig}/{s}/build/{s}-complex.top'.format(lig=self.ligand, s=system)]
-            files += ['systems/ties{lig}/{s}/build/{s}-tags.pdb'.format(lig=self.ligand, s=system)]
-        print "Input files:", files
+        files += ['systems/ties{lig}/{s}/build/{s}-complex.pdb'.format(lig=self.ligand, s=self.system)]
+        files += ['systems/ties{lig}/{s}/build/{s}-complex.top'.format(lig=self.ligand, s=self.system)]
+        files += ['systems/ties{lig}/{s}/build/{s}-tags.pdb'.format(lig=self.ligand, s=self.system)]
         return files
 
     @property
     def replicas(self):
-        return self.number_of_replicas*len(self.lambdas)*len(self.systems)
+        return self.number_of_replicas*len(self.lambdas)*len(self.system)
