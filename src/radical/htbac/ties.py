192a42018c5df225d27660b3e6cec6ef4bd40cf7
Jumana Dakka <jumanadakka@gmail.com>
2018-03-21 13:00:07 -0400

    setup for additional fifo experiments

--- src/radical/htbac/ties.py
+++ src/radical/htbac/ties.py
@@ -100,52 +100,52 @@ class Ties(object):
             
         # Analysis stage
         # ==============
-        analysis = Stage()
-        analysis.name = 'analysis'
+        # analysis = Stage()
+        # analysis.name = 'analysis'
 
-        for replica in range(self.number_of_replicas):
-            analysis_task = Task()
-            analysis_task.name = 'replica_{}'.format(replica)
+        # for replica in range(self.number_of_replicas):
+        #     analysis_task = Task()
+        #     analysis_task.name = 'replica_{}'.format(replica)
 
-            analysis_task.arguments += ['-d', '*ti.out', '>', 'dg_{}.out'.format(analysis_task.name)]
-            analysis_task.executable = [NAMD_TI_ANALYSIS]
+        #     analysis_task.arguments += ['-d', '*ti.out', '>', 'dg_{}.out'.format(analysis_task.name)]
+        #     analysis_task.executable = [NAMD_TI_ANALYSIS]
 
-            analysis_task.mpi = False
-            analysis_task.cores = 1
+        #     analysis_task.mpi = False
+        #     analysis_task.cores = 1
 
-            for p in filter(None, [pipeline, previous_pipeline]):
-                production_stage = next(stage for stage in p.stages if stage.name == 'prod')
-                production_tasks = [t for t in production_stage.tasks if analysis_task.name in t.name]
-                links = ['$Pipeline_{}_Stage_{}_Task_{}/alch_{}_ti.out'.format(p.uid, production_stage.uid, t.uid, t.name.split('_lambda_')[-1]) for t in production_tasks]
-                analysis_task.link_input_data += links
+        #     for p in filter(None, [pipeline, previous_pipeline]):
+        #         production_stage = next(stage for stage in p.stages if stage.name == 'prod')
+        #         production_tasks = [t for t in production_stage.tasks if analysis_task.name in t.name]
+        #         links = ['$Pipeline_{}_Stage_{}_Task_{}/alch_{}_ti.out'.format(p.uid, production_stage.uid, t.uid, t.name.split('_lambda_')[-1]) for t in production_tasks]
+        #         analysis_task.link_input_data += links
 
-            analysis.add_tasks(analysis_task)
+        #     analysis.add_tasks(analysis_task)
 
-        pipeline.add_stages(analysis)
+        # pipeline.add_stages(analysis)
 
-        # Averaging stage
-        # ===============
-        average = Stage()
-        average.name = 'average'
+        # # Averaging stage
+        # # ===============
+        # average = Stage()
+        # average.name = 'average'
 
-        average_task = Task()
-        average_task.name = 'average_dg'
-        average_task.arguments = ['-1 --quiet dg_* > dgs.out']  # .format(pipeline.uid)]
-        average_task.executable = ['head']
+        # average_task = Task()
+        # average_task.name = 'average_dg'
+        # average_task.arguments = ['-1 --quiet dg_* > dgs.out']  # .format(pipeline.uid)]
+        # average_task.executable = ['head']
 
-        average_task.mpi = False
-        average_task.cores = 1
+        # average_task.mpi = False
+        # average_task.cores = 1
 
-        previous_stage = pipeline.stages[-1]
-        previous_tasks = previous_stage.tasks
+        # previous_stage = pipeline.stages[-1]
+        # previous_tasks = previous_stage.tasks
 
-        links = ['$Pipeline_{}_Stage_{}_Task_{}/dg_{}.out'.format(pipeline.uid, previous_stage.uid, t.uid,
-                                                                  t.name) for t in previous_tasks]
-        average_task.link_input_data = links
-        #average_task.download_output_data = ['dgs.out']  # .format(pipeline.uid)]
+        # links = ['$Pipeline_{}_Stage_{}_Task_{}/dg_{}.out'.format(pipeline.uid, previous_stage.uid, t.uid,
+        #                                                           t.name) for t in previous_tasks]
+        # average_task.link_input_data = links
+        # #average_task.download_output_data = ['dgs.out']  # .format(pipeline.uid)]
 
-        average.add_tasks(average_task)
-        pipeline.add_stages(average)
+        # average.add_tasks(average_task)
+        # pipeline.add_stages(average)
 
 
         print 'TIES pipeline has', len(pipeline.stages), 'stages. Tasks counts:', [len(s.tasks) for s in pipeline.stages]
--- testing_EoP/esmacs_2_pipelines.py
+++ testing_EoP/esmacs_2_pipelines.py
@@ -5,15 +5,15 @@ def main():
 
     ht = Runner()
 
-    esm1 = Esmacs(number_of_replicas=1, system='brd-gsk2', cores=32, full=False)
-    esm2 = Esmacs(number_of_replicas=1, system='brd4-gsk3', cores=32, full=False)
+    esm1 = Esmacs(number_of_replicas=1, system='brd4-gsk1', cores=32, full=False)
+    esm2 = Esmacs(number_of_replicas=1, system='brd4-gsk2', cores=32, full=False)
 
     ht.add_protocol(esm1)
     ht.add_protocol(esm2)
 
 
-    ht.rabbitmq_config(hostname='two.radical-project.org', port=32964) # add new port number 
-    ht.run(walltime=90)
+    ht.rabbitmq_config(hostname='two.radical-project.org', port=33072) # add new port number 
+    ht.run(walltime=120)
 
 
 if __name__ == '__main__':
