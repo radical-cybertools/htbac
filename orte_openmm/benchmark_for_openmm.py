caf384df4c9aa300dd080cd065a0e7c9da9620d6
Jumana Dakka <jumanadakka@gmail.com>
2018-03-23 15:28:28 -0400

    benchmark script for openmm

---
+++ orte_openmm/benchmark_for_openmm.py
@@ -0,0 +1,88 @@
+from radical.entk import Pipeline, Stage, Task, AppManager, ResourceManager
+import os
+
+# ------------------------------------------------------------------------------
+# Set default verbosity
+
+if os.environ.get('RADICAL_ENTK_VERBOSE') == None:
+    os.environ['RADICAL_ENTK_VERBOSE'] = 'INFO'
+
+
+def generate_pipeline():
+    
+    # Create a Pipeline object
+    p = Pipeline()
+    p.name = 'p1'
+
+    # Create a Stage object 
+    s1 = Stage()
+    s1.name = 's1'
+
+    # Create a Task object which creates a file named 'output.txt' of size 1 MB
+    task = Task()  
+    task.name = 't1' 
+    task.pre_exec = ['export PATH="/lustre/atlas/scratch/farkaspall/chm126/miniconda3/bin:$PATH"',
+                    'export HOME=/lustre/atlas/scratch/farkaspall/chm126',
+                    'export MINICONDA3="$HOME/miniconda3"',
+                    'export PATH="$MINICONDA3/bin:$PATH"',
+                    'export LD_LIBRARY_PATH=$MINICONDA3/lib:$LD_LIBRARY_PATH']
+     
+    task.mpi = False 
+    task.cores = 16
+    task.arguments = ['benchmark.py']
+    task.executable = ['python -m']
+
+    # Add the Task to the Stage
+    s1.add_tasks(task)
+
+    # Add Stage to the Pipeline
+    p.add_stages(s1)
+
+
+
+
+if __name__ == '__main__':
+
+
+    # Create a dictionary describe four mandatory keys:
+    # resource, walltime, cores and project
+    # resource is 'local.localhost' to execute locally
+    res_dict = {
+
+            'resource': 'ornl.titan_orte',
+            'walltime': 120,
+            'cores': 16,
+            'project': 'chm126',
+            'queue': 'batch',
+            'access_schema': 'local'
+    }
+
+
+
+    # Create Resource Manager object with the above resource description
+    rman = ResourceManager(res_dict)
+
+    # Create Application Manager
+    appman = AppManager(autoterminate=False)
+
+    # Assign resource manager to the Application Manager
+    appman.resource_manager = rman
+
+    p = generate_pipeline()
+    
+    # Assign the workflow as a set of Pipelines to the Application Manager
+    appman.assign_workflow(set([p]))
+
+    # Run the Application Manager
+    appman.run()
+
+    p = generate_pipeline()
+    #print p.uid
+
+    # Assign the workflow as a set of Pipelines to the Application Manager
+    appman.assign_workflow(set([p]))
+
+    # Run the Application Manager
+    appman.run()
+
+    appman.resource_terminate()
