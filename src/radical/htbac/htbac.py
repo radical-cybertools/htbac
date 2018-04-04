cc266ce85eb79769ea6b4a5f03d5583a38884773
Jumana Dakka <jumanadakka@gmail.com>
2018-04-04 09:24:04 -0400

    poE implementation for BW retesting of TIES

--- src/radical/htbac/htbac.py
+++ src/radical/htbac/htbac.py
@@ -17,7 +17,6 @@ class Runner(object):
         self.ids = None
         self.app_manager = None
         self.total_replicas = 0
-        self._cores = 0
 
         # Profiler for Runner
         self._uid = ru.generate_id('radical.htbac.workflow_runner')
@@ -30,26 +29,14 @@ class Runner(object):
     def add_protocol(self, protocol):
         self._protocols.append(protocol)
 
-    # @property
-    # def cores(self):
-    #     return self._cores
-
-    # @cores.setter
-    # def cores(self, val):
-    #     if isinstance(val, int):
-    #         self._cores = val
-    #     else:
-    #         raise TypeError()
-
     def rabbitmq_config(self, hostname='localhost', port=5672):
         self._hostname = hostname
         self._port = port
 
-
-    def run(self, strong_scaled=1, autoterminate=True, queue='high', walltime=1440):
+    def run(self, strong_scaled=1):
         pipelines = set()
         input_data = list()
-
+        
         for protocol in self._protocols:
             gen_pipeline = protocol.generate_pipeline()
             pipelines.add(gen_pipeline)
@@ -57,24 +44,18 @@ class Runner(object):
             self.ids[protocol.id()] = gen_pipeline
             # protocol.id is the uuid, gen_pipeline.uid is the pipeline
 
-            # self.total_replicas += protocol.replicas
+            self.total_replicas += protocol.replicas
             self._cores += protocol.total_cores
-        #self._cores = self._cores * self.total_replicas
+
+        # self._cores = self._cores * self.total_replicas
         print 'Running on', self._cores, 'cores.'
 
         res_dict = {'resource': 'ncsa.bw_aprun',
-                    'walltime': walltime,
+                    'walltime': 180,
                     'cores': int(self._cores*strong_scaled),
                     'project': 'bamm',
-                    'queue': queue,
+                    'queue': 'high',
                     'access_schema': 'gsissh'}
-
-        # res_dict = {'resource': 'ornl.titan_aprun',
-        #             'walltime': walltime,
-        #             'cpus': int(self._cores*strong_scaled),
-        #             'project': 'CHM126',
-        #             'queue': queue,
-        #             'access_schema': 'local'}
 
         # Create Resource Manager object with the above resource description
         resource_manager = ResourceManager(res_dict)
@@ -88,28 +69,3 @@ class Runner(object):
         self._prof.prof('execution_run')
         print 'Running...'
         self.app_manager.run()    # this method is blocking until all pipelines show state = completed
-
-
-    def rerun(self, protocol=None, terminate=True, previous_pipeline=None):
-
-        if self.ids.get(previous_pipeline.id(), None) is not None:
-
-            pipelines = set()
-
-            gen_pipeline = protocol.generate_pipeline(previous_pipeline=self.ids[previous_pipeline.id()])
-
-            pipelines.add(gen_pipeline)
-
-            self.ids[protocol.id()] = gen_pipeline
-
-            self.app_manager.assign_workflow(pipelines)
-
-            self.app_manager.run()
-
-            if terminate:
-                self.app_manager.resource_terminate()
-
-        else: 
-
-            print "ERROR: previous protocol instance is not found"
-
--- src/radical/htbac/ties.py
+++ src/radical/htbac/ties.py
@@ -15,18 +15,17 @@ _full_steps = dict(min=1000, eq1=30000, eq2=970000, prod=2000000)
 
 class Ties(object):
 
-    def __init__(self, number_of_replicas, number_of_windows, system, cores=32,
-                workflow=None, ligand=False, full=False, additional=list()):
+    def __init__(self, number_of_replicas, number_of_windows=0, additional=list(),
+                 systems=list(), workflow=None, cores=32, ligand=False, full=False):
 
         self.number_of_replicas = number_of_replicas
         self.lambdas = np.linspace(0.0, 1.0, number_of_windows, endpoint=True)
         self.lambdas = np.append(self.lambdas, additional or [0.05, 0.95])
         self.ligand = '-ligands' if ligand else ''
-        self.step_count = _full_steps if full else _reduced_steps
-        
-        self.system = system
-        print "Lambda Windows: ", self.lambdas
-        print "step count: ", self.step_count 
+        self.step_count = _reduced_steps
+        print self.step_count
+        self.systems = systems
+        print "Lambda Windows:", self.lambdas
 
         self.cores = cores
         self._id = uuid.uuid1()  # generate id
@@ -56,97 +55,47 @@ class Ties(object):
             stage.name = step
             print stage.name
 
-        
-            box = pmd.amber.AmberAsciiRestart('systems/ties{lig}/{s}/build/{s}-complex.crd'.format(lig=self.ligand, s=self.system)).box
-                
-            for replica in range(self.number_of_replicas):
-                for ld in self.lambdas:
-                
-                    task = Task()
-                    task.name = 'system_{}_replica_{}_lambda_{}'.format(self.system, replica, ld)
+            for system in self.systems:
+                box = pmd.amber.AmberAsciiRestart('systems/ties{lig}/{s}/build/{s}-complex.crd'.format(lig=self.ligand, s=system)).box
+                    
+                for replica in range(self.number_of_replicas):
+                    for ld in self.lambdas:
+                    
+                        task = Task()
+                        task.name = 'system_{}_replica_{}_lambda_{}'.format(system, replica, ld)
 
-                    task.arguments += ['ties-{}.conf'.format(stage.name)]
-                    task.copy_input_data = ['$SHARED/ties-{}.conf'.format(stage.name)]
-                    task.executable = [NAMD2]
+                        task.arguments += ['ties-{}.conf'.format(stage.name)]
+                        task.copy_input_data = ['$SHARED/ties-{}.conf'.format(stage.name)]
+                        task.executable = [NAMD2]
 
-                    task.mpi = True
-                    task.cores = self.cores
+                        task.mpi = True
+                        task.cores = self.cores
 
-                    links = []
-                    links += ['$SHARED/{}-complex.top'.format(self.system), '$SHARED/{}-tags.pdb'.format(self.system)]
-                    if self.workflow.index(step):
-                        previous_stage = pipeline.stages[-1]
-                        previous_task = next(t for t in previous_stage.tasks if t.name == task.name)
-                        path = '$Pipeline_{}_Stage_{}_Task_{}/'.format(pipeline.uid, previous_stage.uid, previous_task.uid)
-                        links += [path+previous_stage.name+suffix for suffix in _simulation_file_suffixes]
-                    else:
-                        links += ['$SHARED/{}-complex.pdb'.format(self.system)]
+                        links = []
+                        links += ['$SHARED/{}-complex.top'.format(system), '$SHARED/{}-tags.pdb'.format(system)]
+                        if self.workflow.index(step):
+                            previous_stage = pipeline.stages[-1]
+                            previous_task = next(t for t in previous_stage.tasks if t.name == task.name)
+                            path = '$Pipeline_{}_Stage_{}_Task_{}/'.format(pipeline.uid, previous_stage.uid, previous_task.uid)
+                            links += [path+previous_stage.name+suffix for suffix in _simulation_file_suffixes]
+                        else:
+                            links += ['$SHARED/{}-complex.pdb'.format(system)]
 
-                    print "Linking files:", links
-                    task.link_input_data = links
+                        #print "Linking files:", links
+                        task.link_input_data = links
 
-                    task.pre_exec += ["sed -i 's/BOX_X/{}/g' *.conf".format(box[0]),
-                                      "sed -i 's/BOX_Y/{}/g' *.conf".format(box[1]),
-                                      "sed -i 's/BOX_Z/{}/g' *.conf".format(box[2]),
-                                      "sed -i 's/SYSTEM/{}/g' *.conf".format(self.system)]
+                        task.pre_exec += ["sed -i 's/BOX_X/{}/g' *.conf".format(box[0]),
+                                          "sed -i 's/BOX_Y/{}/g' *.conf".format(box[1]),
+                                          "sed -i 's/BOX_Z/{}/g' *.conf".format(box[2]),
+                                          "sed -i 's/SYSTEM/{}/g' *.conf".format(system)]
 
-                    task.pre_exec += ["sed -i 's/STEP/{}/g' *.conf".format(self.step_count[step])]
+                        task.pre_exec += ["sed -i 's/STEP/{}/g' *.conf".format(self.step_count[step])]
 
-                    task.pre_exec += ["sed -i 's/LAMBDA/{}/g' *.conf".format(ld)]
+                        task.pre_exec += ["sed -i 's/LAMBDA/{}/g' *.conf".format(ld)]
 
-                    stage.add_tasks(task)
+                        stage.add_tasks(task)
 
             pipeline.add_stages(stage)
-            
-        # Analysis stage
-        # ==============
-        # analysis = Stage()
-        # analysis.name = 'analysis'
-
-        # for replica in range(self.number_of_replicas):
-        #     analysis_task = Task()
-        #     analysis_task.name = 'replica_{}'.format(replica)
-
-        #     analysis_task.arguments += ['-d', '*ti.out', '>', 'dg_{}.out'.format(analysis_task.name)]
-        #     analysis_task.executable = [NAMD_TI_ANALYSIS]
-
-        #     analysis_task.mpi = False
-        #     analysis_task.cores = 1
-
-        #     for p in filter(None, [pipeline, previous_pipeline]):
-        #         production_stage = next(stage for stage in p.stages if stage.name == 'prod')
-        #         production_tasks = [t for t in production_stage.tasks if analysis_task.name in t.name]
-        #         links = ['$Pipeline_{}_Stage_{}_Task_{}/alch_{}_ti.out'.format(p.uid, production_stage.uid, t.uid, t.name.split('_lambda_')[-1]) for t in production_tasks]
-        #         analysis_task.link_input_data += links
-
-        #     analysis.add_tasks(analysis_task)
-
-        # pipeline.add_stages(analysis)
-
-        # # Averaging stage
-        # # ===============
-        # average = Stage()
-        # average.name = 'average'
-
-        # average_task = Task()
-        # average_task.name = 'average_dg'
-        # average_task.arguments = ['-1 --quiet dg_* > dgs.out']  # .format(pipeline.uid)]
-        # average_task.executable = ['head']
-
-        # average_task.mpi = False
-        # average_task.cores = 1
-
-        # previous_stage = pipeline.stages[-1]
-        # previous_tasks = previous_stage.tasks
-
-        # links = ['$Pipeline_{}_Stage_{}_Task_{}/dg_{}.out'.format(pipeline.uid, previous_stage.uid, t.uid,
-        #                                                           t.name) for t in previous_tasks]
-        # average_task.link_input_data = links
-        # #average_task.download_output_data = ['dgs.out']  # .format(pipeline.uid)]
-
-        # average.add_tasks(average_task)
-        # pipeline.add_stages(average)
-
 
         print 'TIES pipeline has', len(pipeline.stages), 'stages. Tasks counts:', [len(s.tasks) for s in pipeline.stages]
         return pipeline
@@ -155,14 +104,17 @@ class Ties(object):
     @property
     def input_data(self):
         files = ['default_configs/ties-{}.conf'.format(step) for step in self.workflow]
-        files += ['systems/ties{lig}/{s}/build/{s}-complex.pdb'.format(lig=self.ligand, s=self.system)]
-        files += ['systems/ties{lig}/{s}/build/{s}-complex.top'.format(lig=self.ligand, s=self.system)]
-        files += ['systems/ties{lig}/{s}/build/{s}-tags.pdb'.format(lig=self.ligand, s=self.system)]
+        for system in self.systems:
+            files += ['systems/ties{lig}/{s}/build/{s}-complex.pdb'.format(lig=self.ligand, s=system)]
+            files += ['systems/ties{lig}/{s}/build/{s}-complex.top'.format(lig=self.ligand, s=system)]
+            files += ['systems/ties{lig}/{s}/build/{s}-tags.pdb'.format(lig=self.ligand, s=system)]
+        print "Input files:", files
         return files
 
     @property
     def replicas(self):
-        return self.number_of_replicas*len(self.lambdas)*len(self.system)
-     @property
+        return self.number_of_replicas*len(self.lambdas)*len(self.systems)
+
+    @property
     def total_cores(self):
-        return self.number_of_replicas*len(self.lambdas)*self.cores
+        return self.number_of_replicas*len(self.lambdas)*len(self.systems)*self.cores
