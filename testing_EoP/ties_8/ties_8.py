7745a90fe955321f781ea43defae4ab94252c4a8
Jumana Dakka <jumanadakka@gmail.com>
2018-04-07 15:14:49 -0400

    fixed core count

--- testing_EoP/ties_8/ties_8.py
+++ testing_EoP/ties_8/ties_8.py
@@ -5,14 +5,14 @@ def main():
 
     ht = Runner()
 
-    ties1 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-1-2')
-    ties2 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-1-3')
-    ties3 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-1-4')
-    ties4 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-1-5')
-    ties5 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-4-2')
-    ties6 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-4-3')
-    ties7 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-4-4')
-    ties8 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-4-5')
+    ties1 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-1-2', cores=32, full=False)
+    ties2 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-1-3', cores=32, full=False)
+    ties3 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-1-4', cores=32, full=False)
+    ties4 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-1-5', cores=32, full=False)
+    ties5 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-4-2', cores=32, full=False)
+    ties6 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-4-3', cores=32, full=False)
+    ties7 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-4-4', cores=32, full=False)
+    ties8 = Ties(number_of_replicas=5, number_of_windows=11, system='brd4-gsk3-4-5', cores=32, full=False)
 
     ht.add_protocol(ties1)
     ht.add_protocol(ties2)
