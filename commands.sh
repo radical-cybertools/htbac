45c90d7bad4475a2e8ee70614ffbe023cefed209
Jumana <jdakka@two.radical-project.org>
2018-04-16 17:46:56 +0200

    commands for VM testing

---
+++ commands.sh
@@ -0,0 +1,12 @@
+
+export RADICAL_PILOT_DBURL=mongodb://htbac:htbac@ds251287.mlab.com:51287/htbac-inspire-1
+export SAGA_PTY_SSH_TIMEOUT=2000
+export RADICAL_PILOT_PROFILE=True
+export RADICAL_ENMD_PROFILE=True
+export RADICAL_ENMD_PROFILING=1
+export RP_ENABLE_OLD_DEFINES=True
+export PATH=/usr/sbin:$PATH
+export RADICAL_VERBOSE="DEBUG"
+
+myproxy-logon -l dakka -s tfca.ncsa.illinois.edu
+
