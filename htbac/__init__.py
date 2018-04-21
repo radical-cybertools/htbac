cbb200df4da1b22927c29c482746a82e06ac65ad
Kristof Farkas-Pall <kristof.farkas-pall.14@ucl.ac.uk>
2018-04-22 00:38:12 +0100

    Folder structure refactor

--- htbac/__init__.py
+++ htbac/__init__.py
@@ -1,12 +1,7 @@
-# Version
-from .version import version, __version__
-
 from .htbac import Runner
-from .esmacs import Esmacs
-from .ties import Ties
+from radical.htbac.protocols.esmacs import Esmacs
 from .ties_eq import TiesEquilibration
-from .ties_prod import TiesProduction
-from .ties_full_analysis import TiesAnalysis, AdaptiveQuadrature
+from radical.htbac.protocols.ties_prod import TiesProduction
 
 from .simulation import BaseSimulation, EnsembleSimulation
 from .system import System
