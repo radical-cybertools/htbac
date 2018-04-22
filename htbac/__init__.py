ff9fffe70ca9959f385374dc227b0f5788358d87
Kristof Farkas-Pall <kristof.farkas-pall.14@ucl.ac.uk>
2018-04-22 14:55:10 +0100

    Fix imports

--- htbac/__init__.py
+++ htbac/__init__.py
@@ -1,7 +1,7 @@
 from .htbac import Runner
-from radical.htbac.protocols.esmacs import Esmacs
-from .ties_eq import TiesEquilibration
-from radical.htbac.protocols.ties_prod import TiesProduction
+# from radical.htbac.protocols.esmacs import Esmacs
+# from .ties_eq import TiesEquilibration
+# from radical.htbac.protocols.ties_prod import TiesProduction
 
 from .simulation import BaseSimulation, EnsembleSimulation
 from .system import System
