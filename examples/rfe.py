7afdc55f0d5ca8e503829f605e6848e01ecf666a
Jumana Dakka <jumanadakka@gmail.com>
2018-06-25 19:39:25 -0400

    example workload for da_scheduler

---
+++ examples/rfe.py
@@ -0,0 +1,48 @@
+"""Relative free energy calculations
+
+Example implementation using Abigail and NAMD.
+
+"""
+
+import numpy as np
+
+from htbac import Runner, System, Simulation, Protocol, AbFile
+from htbac.protocols import Rfe
+
+
+def run_rfe():
+    pdb = AbFile('systems/ptp1b-l1-l2-complex.pdb', tag='pdb')
+    top = AbFile('systems/ptp1b-l1-l2-complex.top', tag='topology')
+    tag = AbFile('systems/ptp1b-l1-l2-tags.pdb', tag='alchemicaltags')
+    cor = AbFile('systems/ptp1b-l1-l2-complex.inpcrd', tag='coordinate')
+    system = System(name='ptp1b-l1-l2', files=[pdb, top, tag, cor])
+
+    p = Protocol(clone_settings=False)
+
+    for step, numsteps in zip(Rfe.steps, [10000, 3000000]):
+
+        rfe = Simulation()
+        rfe.system = system
+        rfe.engine = 'namd_mpi'
+        rfe.cores = 128
+
+        rfe.cutoff = 12.0
+        rfe.switchdist = 10.0
+        rfe.pairlistdist = 13.5
+        rfe.numminsteps = 10000
+        rfe.numsteps = numsteps
+
+        rfe.add_input_file(step, is_executable_argument=True)
+
+        rfe.add_ensemble('replica', range(5))
+        rfe.add_ensemble('lambdawindow', np.linspace(0, 1, 65))
+
+        p.append(rfe)
+
+    ht = Runner('bw_aprun', comm_server=('two.radical-project.org', 33158))
+    ht.add_protocol(p)
+    ht.run(walltime=480, queue='high')
+
+
+if __name__ == '__main__':
+    run_rfe()
