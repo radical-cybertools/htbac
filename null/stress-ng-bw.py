6fee97b57e2fae4f09dcf0b3dc0b108852f9971b
Jumana Dakka <jumanadakka@gmail.com>
2018-07-19 18:03:44 -0400

    stress-ng null workload

---
+++ null/stress-ng-bw.py
@@ -0,0 +1,88 @@
+
+import os
+from radical.entk import Pipeline, Stage, Task
+from radical.entk import AppManager
+
+# ------------------------------------------------------------------------------
+# Set default verbosity
+if os.environ.get('RADICAL_ENTK_VERBOSE') == None:
+    os.environ['RADICAL_ENTK_VERBOSE'] = 'INFO'
+
+
+# Description of how the RabbitMQ process is accessible
+# No need to change/set any variables if you installed RabbitMQ has a system
+# process. If you are running RabbitMQ under a docker container or another
+# VM, set "RMQ_HOSTNAME" and "RMQ_PORT" in the session where you are running
+# this script.
+hostname = os.environ.get('two.radical-project.org', '33048')
+port = os.environ.get('two.radical-project.org', 33048)
+
+if __name__ == '__main__':
+
+    pipelines = set()
+    p1 = Pipeline()
+    p2 = Pipeline()
+    
+    for i in range(4):
+        s1 = Stage()
+
+        for x in range(10):
+            t = Task()
+            t.name = 'my-task'        # Assign a name to the task (optional, do not use ',' or '_')
+            t.cpu_reqs = { 
+                            'processes': 1,
+                            'process_type': None,
+                            'threads_per_process': 32,
+                            'thread_type': None
+                        }
+            t.pre_exec  = ['export PATH=/u/sciteam/dakka/stress-ng/stress-ng-0.09.34:$PATH']
+            t.executable = ['stress-ng']   # Assign executable to the task   
+            t.arguments = ['-c', '32', '-t', '600']  # Assign arguments for the task executable
+            s1.add_tasks(t)
+
+    for i in range(4):
+        s2 = Stage()
+
+        for x in range(10):
+            t = Task()
+            t.name = 'my-task'        # Assign a name to the task (optional, do not use ',' or '_')
+            t.cpu_reqs = { 
+                            'processes': 1,
+                            'process_type': None,
+                            'threads_per_process': 32,
+                            'thread_type': None
+                        }
+        
+            t.pre_exec  = ['export PATH=/u/sciteam/dakka/stress-ng/stress-ng-0.09.34:$PATH']
+            t.executable = ['stress-ng']   # Assign executable to the task   
+            t.arguments = ['-c', '32', '-t', '600']  # Assign arguments for the task executable
+            s2.add_tasks(t)
+
+        p1.add_stages(s1)
+        p2.add_stages(s2)
+
+    pipelines.add(p1)
+    pipelines.add(p2) 
+  
+   
+    
+    # appman = AppManager(hostname='two.radical-project.org', port=33048)
+    amgr = AppManager(hostname='two.radical-project.org', port=33048)
+    amgr.resource_desc = {'resource': 'ncsa.bw_aprun',
+                            'walltime': 30,
+                            'cpus': 640,
+                            'project': 'bamm',
+                            'queue': 'high',
+                            'access_schema': 'gsissh'}
+    amgr.workflow = pipelines
+
+    # Assign resource request description to the Application Manager
+    # resource_manager = ResourceManager(res_dict)
+    # appman.resource_manager = resource_manager
+
+    # Assign the workflow as a set or list of Pipelines to the Application Manager
+    # Note: The list order is not guaranteed to be preserved
+    # appman.assign_workflow(pipelines)
+
+    # Run the Application Manager
+    amgr.run()
