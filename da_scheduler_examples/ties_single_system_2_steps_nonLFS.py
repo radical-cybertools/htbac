81e3218048cd627f9d183ae1ed4451f20881e941
Jumana Dakka <jumanadakka@gmail.com>
2018-07-30 11:17:09 -0400

    more examples

--- da_scheduler_examples/ties_single_system_2_steps_nonLFS.py
+++ da_scheduler_examples/ties_single_system_2_steps_nonLFS.py
@@ -1,16 +1,16 @@
-from radical.htbac import Ties, Runner
-import os 
+from radical.htbac import Ties_nonLFS, Runner
+
 
 def main():
 
     ht = Runner()
 
-    ties = Ties(number_of_replicas=5, number_of_windows=11, systems=['brd4-gsk3-1'], full=True)
+    ties = Ties_nonLFS(number_of_replicas=5, number_of_windows=3, systems=['brd4-gsk3-1'], full=True)
                                                                   
 
     ht.add_protocol(ties)
     ht.cores = 32
-    ht.rabbitmq_config(hostname='two.radical-project.org', port=33048)
+    ht.rabbitmq_config(hostname='two.radical-project.org', port=33130)
     ht.run(walltime = 40)
 
 
@@ -21,5 +21,5 @@ if __name__ == '__main__':
     os.environ['RADICAL_ENTK_PROFILE'] = True
     os.environ['RADICAL_VERBOSE'] = True
     os.environ['PATH'] = '/usr/sbin:$PATH'
-    
+
     main()
