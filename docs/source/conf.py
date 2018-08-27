9bc49d9766a332fa8eafb91d666f0248637164ea
Jumana Dakka <jumanadakka@gmail.com>
2018-08-27 12:16:38 -0400

    added flags to match RCT

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,13 +82,14 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'armstrong'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {"collapsiblesidebar" : "true"}
+html_theme_path = ["_themes", ]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -140,6 +141,7 @@ latex_documents = [
      u'htbac', 'manual'),
 ]
 
+latex_show_pagerefs = True
 
 # -- Options for manual page output ------------------------------------------
 
